b48d251de1dedd592bac85d99182e2001ea9801e
Guilherme <gui-teixeirapimentel@hotmail.com>
2023-12-08 11:01:28 -0300

    Added obj model loading

--- src/main.cpp
+++ src/main.cpp
@@ -15,28 +15,28 @@
 #include <cmath>
 #include <ranges>
 #include <cassert>
-
+#include <fstream>
 bool quited = false;
 
-template<typename T>
+template <typename T>
 struct Matrix3D
 {
     T data[3][3];
 };
 
-template<typename T>
+template <typename T>
 struct Point
 {
     T x;
     T y;
 };
 
-template<typename T>
+template <typename T>
 struct Vector3D
 {
     Point<T> as2DPoint() const
     {
-        return Point<T>{.x=x, .y=y};
+        return Point<T>{.x = x, .y = y};
     }
 
     T x;
@@ -49,103 +49,238 @@ using Vector3DFloat = Vector3D<float>;
 using Matrix3DI32 = Matrix3D<int32_t>;
 using PointInt32 = Point<int32_t>;
 
-Vector3DI32 toScreenSpace(const Vector3DFloat& vec, int32_t screenWidth, int32_t screenHeight)
+Vector3DFloat readVector(const std::string_view &line)
 {
-    return {
-        .x = static_cast<int32_t>((vec.x + 1.0f) * (screenWidth/2.0f)),
-        .y = static_cast<int32_t>((-vec.y + 1.0f) * (screenHeight/2.0f)),
-        .z = static_cast<int32_t>(vec.z)
-    };
+    auto currSpacePos = line.find_first_of(' ');
+
+    float v[4] = {};
+    size_t i = 0;
+
+    while (currSpacePos != std::string::npos)
+    {
+        auto nextSpacePos = line.find_first_of(' ', currSpacePos + 1);
+        const auto end = nextSpacePos != std::string::npos ? line.begin() + nextSpacePos : line.end();
+        std::string buffer{line.begin() + currSpacePos, end};
+        float val = (float)atof(buffer.c_str());
+        v[i] = val;
+        i++;
+        currSpacePos = nextSpacePos;
+        assert(i < sizeof(v));
+    }
+
+    return {.x = v[0], .y = v[1], .z = v[2]};
 }
 
-Vector3DFloat applyNonOrthoProj(const Vector3DFloat& vec)
+std::tuple<int, int, int> readFace(const std::string_view &line)
+{
+    auto currSpacePos = line.find_first_of(' ');
+
+    int f[4] = {};
+    size_t i = 0;
+
+    while (currSpacePos != std::string::npos)
+    {
+        auto nextSpacePos = line.find_first_of(' ', currSpacePos + 1);
+        const auto end = nextSpacePos != std::string::npos ? line.begin() + nextSpacePos : line.end();
+        std::string buffer{line.begin() + currSpacePos, end};
+        int val = atoi(buffer.c_str());
+        assert(val > 0);
+        f[i] = val - 1;
+        i++;
+        currSpacePos = nextSpacePos;
+        assert(i < sizeof(f));
+    }
+
+    return {f[0], f[1], f[2]};
+}
+
+std::vector<Vector3DFloat> loadObjFile(const std::string &filename)
+{
+    std::ifstream objFile{filename};
+
+    if (!objFile.is_open())
+    {
+        std::cout << "Could not open file " << filename << std::endl;
+        return {};
+    }
+
+    std::vector<Vector3DFloat> allVertices;
+    std::vector<std::tuple<int, int, int>> allFaces;
+
+    std::string linebuffer;
+    while (std::getline(objFile, linebuffer))
+    {
+        if (linebuffer[0] == 'v')
+        {
+            allVertices.emplace_back(readVector(linebuffer));
+        }
+        else if (linebuffer[0] == 'f')
+        {
+            allFaces.emplace_back(readFace(linebuffer));
+        }
+        // empty line
+        else if (linebuffer.size() < 2)
+        {
+            continue;
+        }
+        else
+        {
+            assert(0 && "Not implemented");
+        }
+    }
+
+    std::vector<Vector3DFloat> res;
+
+    for (const auto &[a, b, c] : allFaces)
+    {
+        assert(a < (int)allVertices.size());
+        assert(b < (int)allVertices.size());
+        assert(c < (int)allVertices.size());
+
+        res.push_back(allVertices[a]);
+        res.push_back(allVertices[b]);
+        res.push_back(allVertices[c]);
+    }
+
+    return res;
+}
+
+Vector3DI32 toScreenSpace(const Vector3DFloat &vec, int32_t screenWidth, int32_t screenHeight)
+{
+    return {
+        .x = static_cast<int32_t>((vec.x + 1.0f) * (screenWidth / 2.0f)),
+        .y = static_cast<int32_t>((-vec.y + 1.0f) * (screenHeight / 2.0f)),
+        .z = static_cast<int32_t>(vec.z)};
+}
+
+Vector3DFloat applyNonOrthoProj(const Vector3DFloat &vec)
 {
     return {
         .x = vec.x / vec.z,
         .y = vec.y / vec.z,
-        .z = vec.z
-    };
+        .z = vec.z};
 }
+
+std::vector<Vector3DFloat> getTranslated(const Vector3DFloat &transl, const std::vector<Vector3DFloat> &vertices)
+{
+    std::vector<Vector3DFloat> res = vertices;
+
+    for (auto &vertice : res)
+    {
+        vertice.x += transl.x;
+        vertice.y += transl.y;
+        vertice.z += transl.z;
+    }
+
+    return res;
+}
+
+std::vector<Vector3DFloat> getRotatedZ(float angle, const std::vector<Vector3DFloat> &vertices)
+{
+    const auto cosangle = cosf(angle);
+    const auto sinangle = sinf(angle);
+
+    std::vector<Vector3DFloat> res = vertices;
+
+    for (auto &vertice : res)
+    {
+        const auto nx = cosangle * vertice.x - sinangle * vertice.y;
+        const auto ny = sinangle * vertice.x + cosangle * vertice.y;
+
+        vertice.x = nx;
+        vertice.y = ny;
+    }
+
+    return res;
+}
+
+std::vector<Vector3DFloat> getRotatedX(float angle, const std::vector<Vector3DFloat> &vertices)
+{
+    const auto cosangle = cosf(angle);
+    const auto sinangle = sinf(angle);
+
+    std::vector<Vector3DFloat> res = vertices;
+
+    for (auto &vertice : res)
+    {
+        const auto ny = cosangle * vertice.y - sinangle * vertice.z;
+        const auto nz = sinangle * vertice.y + cosangle * vertice.z;
+
+        vertice.y = ny;
+        vertice.z = nz;
+    }
+
+    return res;
+}
+
+std::vector<Vector3DFloat> getRotatedY(float angle, const std::vector<Vector3DFloat> &vertices)
+{
+    const auto cosangle = cosf(angle);
+    const auto sinangle = sinf(angle);
+
+    std::vector<Vector3DFloat> res = vertices;
+
+    for (auto &vertice : res)
+    {
+        const auto nx = cosangle * vertice.x + sinangle * vertice.z;
+        const auto nz = -sinangle * vertice.x + cosangle * vertice.z;
+
+        vertice.x = nx;
+        vertice.z = nz;
+    }
+
+    return res;
+}
+
+struct ObjModel
+{
+    static ObjModel fromObjFile(const std::string &filename)
+    {
+        return ObjModel{loadObjFile(filename)};
+    }
+    ObjModel getTranslated(const Vector3DFloat &transl)
+    {
+        return ObjModel{::getTranslated(transl, vertices)};
+    }
+
+    ObjModel getRotatedZ(float angle)
+    {
+        return ObjModel{::getRotatedZ(angle, vertices)};
+    }
+
+    ObjModel getRotatedX(float angle)
+    {
+        return ObjModel{::getRotatedX(angle, vertices)};
+    }
+
+    ObjModel getRotatedY(float angle)
+    {
+        return ObjModel{::getRotatedY(angle, vertices)};
+    }
+
+    std::vector<Vector3DFloat> vertices = {};
+};
 
 struct CubeModel
 {
-    CubeModel getTranslated(const Vector3DFloat& transl)
+    CubeModel getTranslated(const Vector3DFloat &transl)
     {
-        CubeModel res{
-            .vertices = this->vertices
-        };
-
-        for(auto& vertice : res.vertices)
-        {
-            vertice.x += transl.x;
-            vertice.y += transl.y;
-            vertice.z += transl.z;
-        }
-
-        return res;
+        return CubeModel{::getTranslated(transl, vertices)};
     }
 
     CubeModel getRotatedZ(float angle)
     {
-        const auto cosangle = cosf(angle);
-        const auto sinangle = sinf(angle);
-
-        CubeModel res {
-            this->vertices
-        };
-
-        for(auto& vertice : res.vertices)
-        {
-            const auto nx = cosangle*vertice.x - sinangle * vertice.y;
-            const auto ny = sinangle*vertice.x + cosangle * vertice.y;
-
-            vertice.x = nx;
-            vertice.y = ny;
-        }
-
-        return res;
+        return CubeModel{::getRotatedZ(angle, vertices)};
     }
 
     CubeModel getRotatedX(float angle)
     {
-        const auto cosangle = cosf(angle);
-        const auto sinangle = sinf(angle);
-
-        CubeModel res {
-            this->vertices
-        };
-
-        for(auto& vertice : res.vertices)
-        {
-            const auto ny = cosangle*vertice.y - sinangle * vertice.z;
-            const auto nz = sinangle*vertice.y + cosangle * vertice.z;
-
-            vertice.y = ny;
-            vertice.z = nz;
-        }
-
-        return res;
+        return CubeModel{::getRotatedX(angle, vertices)};
     }
 
     CubeModel getRotatedY(float angle)
     {
-        const auto cosangle = cosf(angle);
-        const auto sinangle = sinf(angle);
-
-        CubeModel res {
-            this->vertices
-        };
-
-        for(auto& vertice : res.vertices)
-        {
-            const auto nx = cosangle*vertice.x + sinangle * vertice.z;
-            const auto nz = -sinangle*vertice.x + cosangle * vertice.z;
-
-            vertice.x = nx;
-            vertice.z = nz;
-        }
-
-        return res;
+        return CubeModel{::getRotatedY(angle, vertices)};
     }
 
     std::vector<Vector3DFloat> vertices = {
@@ -214,100 +349,99 @@ struct CubeModel
 struct ScreenBuffer
 {
     ScreenBuffer(int32_t width, int32_t height)
-        :
-        m_data(),
-        m_width(width),
-        m_height(height)
+        : m_data(),
+          m_width(width),
+          m_height(height)
     {
-        m_data.resize(width*height);
+        m_data.resize(width * height);
     }
 
     ~ScreenBuffer() = default;
 
-    void fillOutBuffer(void* outbuffer) const
+    void fillOutBuffer(void *outbuffer) const
     {
         memcpy(outbuffer, m_data.data(), m_width * m_height * sizeof(int32_t));
     }
-    
+
     void cleanScreen(int32_t color = 0xFF000000)
     {
-        for(auto& p : m_data)
+        for (auto &p : m_data)
         {
             p = color;
         }
     }
 
-    void drawLine(const PointInt32& point1, const PointInt32& point2, int32_t argb)
+    void drawLine(const PointInt32 &point1, const PointInt32 &point2, int32_t argb)
     {
         drawLine(point1.x, point1.y, point2.x, point2.y, argb);
     }
 
     void drawLine(int32_t x1, int32_t y1, int32_t x2, int32_t y2, int32_t argb)
     {
-        if(x1 == x2)
+        if (x1 == x2)
         {
-            if(y1 > y2)
+            if (y1 > y2)
             {
                 std::swap(x1, x2);
                 std::swap(y1, y2);
             }
-            for(auto y = y1; y < y2; y++)
+            for (auto y = y1; y < y2; y++)
             {
                 putPixel(x1, y, argb);
             }
             return;
         }
 
-        if(y1 == y2)
+        if (y1 == y2)
         {
-            if(x1 > x2)
+            if (x1 > x2)
             {
                 std::swap(x1, x2);
                 std::swap(y1, y2);
             }
-            for(auto x = x1; x < x2; x++)
+            for (auto x = x1; x < x2; x++)
             {
                 putPixel(x, y1, argb);
             }
             return;
         }
 
-        const auto ax = float(y1 - y2)/(x1 - x2);
-        const auto ay = float(x1 - x2)/(y1 - y2);
-        
-        if(fabs(ax) < fabs(ay))
+        const auto ax = float(y1 - y2) / (x1 - x2);
+        const auto ay = float(x1 - x2) / (y1 - y2);
+
+        if (fabs(ax) < fabs(ay))
         {
-            if(x1 > x2)
+            if (x1 > x2)
             {
                 std::swap(x1, x2);
                 std::swap(y1, y2);
             }
-            const auto b = y2 - ax*x2;
+            const auto b = y2 - ax * x2;
 
-            for(auto x = x1; x <= x2; x++)
+            for (auto x = x1; x <= x2; x++)
             {
-                const auto y = static_cast<int32_t>(ax*x + b);
+                const auto y = static_cast<int32_t>(ax * x + b);
                 putPixel(x, y, argb);
             }
         }
         else
         {
-            if(y1 > y2)
+            if (y1 > y2)
             {
                 std::swap(x1, x2);
                 std::swap(y1, y2);
             }
-            const auto b = x2 - ay*y2;
+            const auto b = x2 - ay * y2;
 
-            for(auto y = y1; y <= y2; y++)
+            for (auto y = y1; y <= y2; y++)
             {
-                const auto x = static_cast<int32_t>(ay*y + b);
+                const auto x = static_cast<int32_t>(ay * y + b);
                 putPixel(x, y, argb);
             }
         }
     }
 
-    void putPixel(const PointInt32& point, int32_t color)
+    void putPixel(const PointInt32 &point, int32_t color)
     {
         putPixel(point.x, point.y, color);
     }
@@ -319,7 +453,7 @@ struct ScreenBuffer
         assert(y >= 0);
         assert(y < m_height);
 
-        m_data[x + (y*m_width)] = argb;
+        m_data[x + (y * m_width)] = argb;
     }
 
     int32_t getWidth() const
@@ -346,35 +480,35 @@ void on_delete(Display *display, Window window)
     quited = true;
 }
 
-void drawCube(float anglez, float anglex, float angley, Vector3DFloat pos, int32_t color)
+template <typename Model_T>
+void drawModel(Model_T model, float anglez, float anglex, float angley, Vector3DFloat pos, int32_t color)
 {
-    CubeModel cb;
-    cb = cb.getTranslated({-0.5f, -0.5f, -0.5f});
+    model = model.getRotatedZ(anglez);
+    model = model.getRotatedX(anglex);
+    model = model.getRotatedY(angley);
 
-    cb = cb.getRotatedZ(anglez);
-    cb = cb.getRotatedX(anglex);
-    cb = cb.getRotatedY(angley);
+    model = model.getTranslated(pos);
 
-    cb = cb.getTranslated(pos);
+    assert((model.vertices.size() % 3) == 0);
 
-    assert((cb.vertices.size() % 3) == 0);
-
-    for(auto itr = cb.vertices.begin();
-        itr != cb.vertices.end(); std::advance(itr, 3))
+    for (auto itr = model.vertices.begin();
+         itr != model.vertices.end(); std::advance(itr, 3))
     {
-        const auto& v1 = *itr;
-        const auto& v2 = *(itr + 1);
-        const auto& v3 = *(itr + 2);
+        const auto &v1 = *itr;
+        const auto &v2 = *(itr + 1);
+        const auto &v3 = *(itr + 2);
 
         const auto p1 = toScreenSpace(applyNonOrthoProj(v1),
-            g_screenBuffer.getWidth(), g_screenBuffer.getHeight()).as2DPoint();
-        
+                                      g_screenBuffer.getWidth(), g_screenBuffer.getHeight())
+                            .as2DPoint();
+
         const auto p2 = toScreenSpace(applyNonOrthoProj(v2),
-            g_screenBuffer.getWidth(), g_screenBuffer.getHeight()).as2DPoint();
+                                      g_screenBuffer.getWidth(), g_screenBuffer.getHeight())
+                            .as2DPoint();
 
         const auto p3 = toScreenSpace(applyNonOrthoProj(v3),
-            g_screenBuffer.getWidth(), g_screenBuffer.getHeight()).as2DPoint();
-
+                                      g_screenBuffer.getWidth(), g_screenBuffer.getHeight())
+                            .as2DPoint();
 
         g_screenBuffer.drawLine(p1, p2, color);
         g_screenBuffer.drawLine(p2, p3, color);
@@ -382,13 +516,20 @@ void drawCube(float anglez, float anglex, float angley, Vector3DFloat pos, int32
     }
 }
 
+void drawCube(float anglez, float anglex, float angley, Vector3DFloat pos, int32_t color)
+{
+    auto model = CubeModel{};
+    model = model.getTranslated({-0.5f, -0.5f, -0.5f});
+    drawModel(model, anglez, anglex, angley, pos, color);
+}
+
 float clampAngle(float angle)
 {
-    if(angle > M_PI)
+    if (angle > M_PI)
     {
         angle = -M_PI;
     }
-    else if(angle < -M_PI)
+    else if (angle < -M_PI)
     {
         angle = M_PI;
     }
@@ -440,7 +581,6 @@ int main(int, char **)
     }
     XFlush(display);
 
-    
     const auto imgWidth = g_screenBuffer.getWidth();
     const auto imgHeight = g_screenBuffer.getHeight();
 
@@ -461,6 +601,7 @@ int main(int, char **)
 
     int currColor = 0;
 
+    const auto utahTeaPot = ObjModel::fromObjFile("assets/teapot.obj");
 
     while (!quited)
     {
@@ -498,7 +639,7 @@ int main(int, char **)
             }
             std::cout << "Got event: " << event.type << std::endl;
         }
-        
+
         const auto x = imgWidth / 2;
         const auto y = imgHeight / 2;
 
@@ -520,17 +661,18 @@ int main(int, char **)
         // g_screenBuffer.drawLine({0, 0}, {100, 400}, blueColor);
 
         static float anglez = 0.0f;
-        anglez += 0.01f;
+        // anglez += 0.01f;
         static float anglex = 0.0f;
-        // anglex += 0.02f;
+        anglex += 0.02f;
         static float angley = 0.0f;
-        // angley += 0.03f;
+        // angley += 0.001f;
 
         angley = clampAngle(angley);
         anglex = clampAngle(anglex);
         anglez = clampAngle(anglez);
 
-        drawCube(anglez, anglex, angley, {0.0f, 0.0f, 2.0f}, redColor);
+        // drawCube(anglez, anglex, angley, {0.0f, 0.0f, 2.0f}, redColor);
+        drawModel(utahTeaPot, anglez, anglex, angley, {0.0f, 0.0f, 6.0f}, redColor);
 
         if (frames > 500)
         {
